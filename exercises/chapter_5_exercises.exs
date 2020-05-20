3d26110561449f386facc1ffa5c7c97e472e94b5
Drew Shamrock <drewshamrock@gmail.com>
2020-05-20 13:26:19 -0700

    Chapter 5 Exercise: Functions-4

--- exercises/chapter_5_exercises.exs
+++ exercises/chapter_5_exercises.exs
@@ -1,12 +1,12 @@
 # Chapter 5
 # Exercise: Functions-2
 
-fun2 = fn
-    (0, 0, _c) -> IO.puts "FizzBuzz"
-    (0, _b, _c) -> IO.puts "Fizz"
-    (_a, 0, _c) -> IO.puts "Buzz"
-    (_a, _b, c) -> IO.puts c
-end
+# fun2 = fn
+#     (0, 0, _c) -> IO.puts "FizzBuzz"
+#     (0, _b, _c) -> IO.puts "Fizz"
+#     (_a, 0, _c) -> IO.puts "Buzz"
+#     (_a, _b, c) -> IO.puts c
+# end
 
 # fun2.(0, 0, 1)
 # fun2.(0, 1, 2)
@@ -17,12 +17,21 @@ end
 
 # Exercise: Functions-3
 
-fun3 = fn n -> fun2.(rem(n,3), rem(n,5), n) end
+# fun3 = fn n -> fun2.(rem(n,3), rem(n,5), n) end
 
-fun3.(10)
-fun3.(11)
-fun3.(12)
-fun3.(13)
-fun3.(14)
-fun3.(15)
-fun3.(16)
+# fun3.(10)
+# fun3.(11)
+# fun3.(12)
+# fun3.(13)
+# fun3.(14)
+# fun3.(15)
+# fun3.(16)
+
+#---------------------
+
+# Exercise: Functions-4
+
+prefix = fn str1 -> (fn str2 -> IO.puts "#{str1} #{str2}" end) end
+
+string1 = prefix.("String1")
+final_string = string1.("String2")
